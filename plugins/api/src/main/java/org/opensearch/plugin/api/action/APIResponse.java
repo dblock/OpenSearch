156649c6d963af5686a61261a7252492e56a3b42
dblock <dblock@amazon.com>
2024-01-11 13:09:35 -0500

    POC for a dynamic OpenAPI plugin, #11843.
    Signed-off-by: dblock <dblock@amazon.com>

---
+++ plugins/api/src/main/java/org/opensearch/plugin/api/action/APIResponse.java
@@ -0,0 +1,84 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.plugin.api.action;
+
+import org.opensearch.Build;
+import org.opensearch.Version;
+import org.opensearch.action.main.MainResponse;
+import org.opensearch.core.action.ActionResponse;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.xcontent.ToXContentObject;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.rest.RestController;
+
+import java.io.IOException;
+
+public class APIResponse extends ActionResponse implements ToXContentObject {
+
+    private Version version;
+    private Build build;
+    private RestController restController;
+
+    APIResponse() {
+
+    }
+
+    APIResponse(StreamInput in) throws IOException {
+        super(in);
+        version = in.readVersion();
+        build = in.readBuild();
+    }
+
+    APIResponse(Version version, Build build) {
+        this.version = version;
+        this.build = build;
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeVersion(version);
+        out.writeBuild(build);
+    }
+
+    public Version getVersion() {
+        return version;
+    }
+
+    public Build getBuild() {
+        return build;
+    }
+
+    public void setRestController(RestController restController) {
+        this.restController = restController;
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+
+        builder.field("openapi", "3.0.1");
+
+        builder.startObject("info")
+            .field("title", build.getDistribution())
+            .field("description", MainResponse.TAGLINE)
+            .field("version", build.getQualifiedVersion())
+            .endObject();
+
+        restController.toXContent(builder, params);
+
+        builder.endObject();
+        return builder;
+    }
+
+    @Override
+    public String toString() {
+        return "APIResponse{" + '\'' + ", version=" + version + '\'' + ", build=" + build + '}';
+    }
+}
---
+++ plugins/api/src/test/java/org/opensearch/plugin/api/APIPluginIT.java
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.plugin.api;
+
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+
+import org.apache.http.util.EntityUtils;
+import org.opensearch.client.Request;
+import org.opensearch.client.Response;
+import org.opensearch.plugins.Plugin;
+import org.opensearch.test.OpenSearchIntegTestCase;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
+
+@ThreadLeakScope(ThreadLeakScope.Scope.NONE)
+@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE)
+public class APIPluginIT extends OpenSearchIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Collections.singletonList(APIPlugin.class);
+    }
+
+    public void testPluginInstalled() throws IOException {
+        Response response = createRestClient().performRequest(new Request("GET", "/_cat/plugins"));
+        String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+        logger.info("response body: {}", body.toString());
+        assertTrue(body.contains("api"));
+    }
+
+    public void testPluginGetAPI() throws IOException {
+        Response response = createRestClient().performRequest(new Request("GET", "/_plugins/api"));
+        String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+        logger.info("response body: {}", body);
+        assertTrue(body.startsWith("{\"openapi\":\"3.0.1\""));
+    }
+}
